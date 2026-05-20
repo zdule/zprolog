381de5d60eb0d7f77c34b6ed16e4c74024081963
Dusan Zivanovic <>
2026-05-21 00:37:40 +0200

    add support for reading from files.

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
+from argparse import ArgumentParser
 from sys import stdin
+from typing import IO
 
 from zprolog.lexer import lex, Peekable
 from zprolog.parser import parse_program, Query, Rule
 from zprolog.program import Program, Query, Rule
 from zprolog.solver import solve
 
-def main():
-    program = Program()
-    for command in parse_program(Peekable(lex(stdin))):
+def process_file(program: Program, f: IO[str]):
+    for command in parse_program(Peekable(lex(f))):
         match command:
             case Query():
-                print("Answer:")
+                print(f"?- {command.term}")
                 for sol in solve(program, command):
                     print(sol)
             case Rule():
                 program.add_rule(command)
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", help="Load program from file")
+    args = parser.parse_args()
+
+    program = Program()
+
+    if args.file:
+        with open(args.file) as f:
+            process_file(program, f)
+    process_file(program, stdin)
 
 if __name__ == "__main__":
     main()
